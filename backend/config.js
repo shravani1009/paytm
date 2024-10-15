33c9caf327ee33514507303ca67d7323ef8dd67a
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-15 22:43:53 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "shrav";
+
+module.exports = JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const mainRouter = require("./router/index");
+const cors = require("cors");
+const mainRouter = require("./routes/index");
+const v2Router = require ("./routes/user")
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1" , mainRouter);
 
 app.use("api/v2", v2Router);
+
+app.listen(3000);
