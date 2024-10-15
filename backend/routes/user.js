af541d70b20ee122182f81f3885bf7dbaaab6a96
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-15 23:49:42 +0530

    initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,45 @@
 const express = require ("express");
-
+const zod = require("zod");
+const { User } = require("../db")
+const JWT_SECRET = require("../config");
+const jwt = require ( "jsonwebtoken")
 const router = express.Router();
 
-router.post('/signup')
+const signupSchema = zod.object({
+    username : zod.string(),
+    password : zod.string(),
+    firstName : zod.string(),
+    password: zod.string()
+    
+})
+
+router.post('/signup', async(req,res)=>{
+    const body = req.body;
+    const { success } = signupSchema.safeParse(req.body);
+    if(!success){
+        res.json({
+            message : "email already taken / incorrect inputs"
+        })
+    }
+
+    const user = User.findOne({
+        username: body.username
+    })
+
+    if (user._id){
+        return res.json({
+            message: "email already taken / choose another email"
+        })
+    }
+
+    const dbUser = await User.create(body);
+    const token = jwt.sign({
+        userId : dbUser._id 
+    } , JWT_SECRET)
+    res.json({
+        message: "account created successfully",
+        token: token
+    })
+})
 
 module.exports = router;
